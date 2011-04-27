ed9eee6134bf54f88f229d2ec6c5e196958d05c6
John Gibb <johngibb@gmail.com>
2011-04-27 14:44:42 -0700

    add preview as a <dt><dd> pair to match rest of page
    All of the other elements are like this:
    
    	<dt><label>Heading</label></dt>
    	<dd>
    		Content under heading
    	</dd>
    
    Because of this structure, it's harder to use
    jQuery selectors to find the previewDiv. Instead,
    I just save it in a local variable.

--- live_markdown_preview.js
+++ live_markdown_preview.js
@@ -1,16 +1,25 @@
 $(function(){
 	var converter = new Showdown.converter();
 	
-	$('dt.previewable + dd textarea')
-		.after(
-			$('<div>')
-				.addClass('markdown-preview')
-				.html('[No Preview]')
+	function createDlItem(dtHtml, ddHtml){
+		return $('<dt>').html(dtHtml).after(
+			$('<dd>').html(ddHtml)
+		)
+	}
+	
+	$('dt.previewable + dd textarea').each(function(){
+		var previewDiv = $('<div>').addClass('markdown-preview').html('[No Preview]');
+		$(this).after(
+			createDlItem(
+				$('<label>').html('Preview'),
+				previewDiv
+			)
 		)
 		.bind('change keyup', function(){
 			var src = $(this).val();
 			var preview = converter.makeHtml(src);
 
-			$(this).next('div.markdown-preview').html(preview);
-		});
-})
+			previewDiv.html(preview);
+		})
+	})
+});
