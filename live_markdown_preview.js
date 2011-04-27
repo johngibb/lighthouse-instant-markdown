b8c7f03cc918497a0e2186a9b2e3289fa68788d9
John Gibb <johngibb@gmail.com>
2011-04-27 14:16:31 -0700

    Watch the textarea, and show contents in preview div
    Currently the preview div is just showing the
    contents of the textarea, unformatted. That's next.

--- live_markdown_preview.js
+++ live_markdown_preview.js
@@ -4,5 +4,11 @@ $(function(){
 			$('<div>')
 				.addClass('markdown-preview')
 				.html('[No Preview]')
-		);
+		)
+		.bind('change keyup', function(){
+			var src = $(this).val();
+			var preview = src; // Todo: Convert markdown to HTML
+
+			$(this).next('div.markdown-preview').html(preview);
+		});
 })
